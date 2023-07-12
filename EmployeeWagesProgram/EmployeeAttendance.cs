b2bb713b7db6e8858bd1951a800d2b9fd92bcf88
S Soundarya <soundarya.s@tvsnext.com>
2023-07-12 10:03:21 +0530

    [Soundarya]Add: Added switchCase.

---
+++ EmployeeWagesProgram/EmployeeAttendance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagesProgram
+{
+    public class employeeprogram
+    {
+        Random random = new Random();
+        public void EmployeeWages()
+        {
+            
+            int empCheck = random.Next(0, 2);
+
+
+            if (empCheck == 0)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+        const int wage_per_hour = 20, FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULL_TIME =0,IS_PART_TIME=1;
+        int totalEmpWage = 0, empHrs = 0;
+
+        public void calculateempwage()
+        {
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = FULL_DAY_HR;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = PART_DAY_HR;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+            }
+            totalEmpWage = wage_per_hour * empHrs;
+           
+            Console.WriteLine(totalEmpWage);
+        }
+    }
+}
---
+++ EmployeeWagesProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeWagesProgram;
+using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage problem");
+
+            employeeprogram e = new employeeprogram();
+            e.EmployeeWages();
+            e.calculateempwage();
+
+        }
+    }
+}
